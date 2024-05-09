2813218d72622d6d2ebdda983f675d370ea12989
mrahman4782 <moharahman50@gmail.com>
2024-05-09 07:42:22 -0400

    Express server + middleware setup

---
+++ backend/middleware/config.js
@@ -0,0 +1,31 @@
+import cors from 'cors';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+
+// Initialize middleware setup
+export const middlewareInit = (app) => {
+
+    app.use(cors());
+    app.use(express.json());
+}
+
+// Setup log directory
+const logDir = path.join(__dirname, 'logs');
+
+// Logger to capture all requests to server
+export const logRequests = morgan('combined', {
+    stream: fs.createWriteStream(path.join(logDir, 'requests.log'), { flags: 'a' })
+})
+
+// Logger to capture valid responses from the server
+export const logSuccess = morgan('combined', {
+    skip: (req, res) => res.statusCode >= 400,
+    stream: fs.createWriteStream(path.join(logDir, 'responses.log'), { flags: 'a' })
+})
+
+// Logger to capture error responses from the server
+export const logFailure = morgan('combined', {
+    skip: (req, res) => res.statusCode < 400,
+    stream: fs.createWriteStream(path.join(logDir, 'errors.log'), { flags: 'a' })
+})
