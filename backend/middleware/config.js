711f40130a1c16e0bc7fd0a1dfda92e041f230b7
melido <enrriquebello10@gmail.com>
2024-05-12 09:47:55 -0400

    includes the usage of fileURLToPath from the url in config

--- backend/middleware/config.js
+++ backend/middleware/config.js
@@ -2,16 +2,25 @@ import cors from 'cors';
 import morgan from 'morgan';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url'; // Import necessary utility from URL module
+import express from 'express'; // Import express
+
+// Get the directory name from the current module URL
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Initialize middleware setup
 export const middlewareInit = (app) => {
-
     app.use(cors());
     app.use(express.json());
 }
 
 // Setup log directory
 const logDir = path.join(__dirname, 'logs');
+
+// Check if log directory exists and create if not
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
 
 // Logger to capture all requests to server
 export const logRequests = morgan('combined', {
--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import React from 'react';
 
-const Home = () => {
+function Home () {
 
     return (
         <h1>
