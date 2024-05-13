31b01dab0eb84f1664dc2b93130c1cb785aec81a
mrahman4782 <moharahman50@gmail.com>
2024-05-13 19:49:58 -0400

    Merged master with feature branch fixing_backend_errors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {middlewareInit, logRequests, logSuccess, logFailure} from './middleware/config.js';
+//import {middlewareInit, logRequests, logSuccess, logFailure} from './middleware/config.js';
 
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
@@ -8,10 +8,10 @@ dotenv.config({ path: "../.env" });
 const app = express();
 const port = 3001;
 
-middlewareInit(app);
-app.use(logRequests);
-app.use(logSuccess);
-app.use(logFailure);
+// middlewareInit(app);
+// app.use(logRequests);
+// app.use(logSuccess);
+// app.use(logFailure);
 
 app.get('/', (req, res) => {
     res.status(200).send(`<h1>Successfully Connected to Server</h1>`);
